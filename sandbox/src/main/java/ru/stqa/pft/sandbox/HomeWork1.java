88a11ee4f12107049115a87df0ce442ac9f68a5d
DaniilAutoQA <daniil-bond@yandex.ru>
2019-12-25 14:44:07 +0300

    изменение функции на метод

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HomeWork1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HomeWork1.java
@@ -4,9 +4,14 @@ public class HomeWork1 {
     public static void main(String[] args) {
         Point point1 = new Point(450,658);
         Point point2 = new Point(120, 356);
-        double result = Point.distance(point1,point2);
+        double result = distance(point1,point2);
     System.out.println("Расстояние между точек х и у равно " + result);
 
+    }
+    public static double distance(Point p1, Point p2) {
+        double dx = p1.x - p2.x;
+        double dy = p1.y - p2.y;
+        return Math.sqrt(dx * dx + dy * dy);
     }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,17 +8,13 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(3,5);
 
-		System.out.println("Area of rectangle = " + area(r));
-		System.out.println("Плащадь квадрата равна " + area(len));
+		System.out.println("Area of rectangle = " + r.area());
+		System.out.println("Плащадь квадрата равна " + len.area());
 	}
 	public static void hello(String name) {
 		System.out.println("Hello " + name);
 
 	}
-	public static double area(Square len){
-		return len.l * len.l;
-	}
-	public static double area(Rectangle r){
-		return r.a * r.b;
-	}
+
+
 }
