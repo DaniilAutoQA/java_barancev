fced9c9d8b73c03d7ac4f318eb13c584954f01b4
DaniilAutoQA <daniil-bond@yandex.ru>
2020-01-06 14:52:13 +0300

    создан перервый тест addressbook
    создан перервый тест addressbook

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,12 +4,12 @@ public class MyFirstProgram {
 	public static void main(String[] arg) {
 		System.out.println("Hello, world!");
 		hello("VAsz");
-		Square len = new Square(5);
+		Square s = new Square(5);
 
 		Rectangle r = new Rectangle(3,5);
 
 		System.out.println("Area of rectangle = " + r.area());
-		System.out.println("Плащадь квадрата равна " + len.area());
+		System.out.println("Плащадь квадрата равна " + s.area());
 	}
 	public static void hello(String name) {
 		System.out.println("Hello " + name);
