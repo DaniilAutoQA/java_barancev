dd9b30e7383d982ef1c1741ca1f2b50963bcb45a
DaniilAutoQA <daniil-bond@yandex.ru>
2020-01-10 16:04:04 +0300

    распределение на классы помощники
    разбиваем класс ApplicationManager разбиваем на несколько классов помощников, разносим вспомогательные фун-ции по этим классам

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,85 +1,44 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+
     WebDriver driver;
-    String baseUrl;
+
+    private NavigationHelper navigationHelper;
+    private GroupHelper groupHelper;
+    private SessionHelper sessionHelper;
 
     public void init() {
         System.setProperty("webdriver.gecko.driver", "D:\\java video\\Баренцев\\geckodriver-v0.26.0-win32\\geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "D:\\java video\\Баренцев\\chromedriver.exe");
         //driver = new ChromeDriver();
         driver = new FirefoxDriver();
-        baseUrl = "http://localhost:8080/addressbook";
+        groupHelper = new GroupHelper(driver);
+        navigationHelper = new NavigationHelper(driver);
+        sessionHelper = new SessionHelper(driver);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.get(baseUrl);
-        login("admin", "secret");
+        driver.get("http://localhost:8080/addressbook");
+        sessionHelper.login("admin", "secret");
     }
 
-    private void login(String username, String password) {
-      driver.findElement(By.name("user")).click();
-      driver.findElement(By.name("user")).clear();
-      driver.findElement(By.name("user")).sendKeys(username);
-      driver.findElement(By.id("LoginForm")).click();
-      driver.findElement(By.name("pass")).click();
-      driver.findElement(By.name("pass")).clear();
-      driver.findElement(By.name("pass")).sendKeys(password);
-      driver.findElement(By.xpath("//input[@value='Login']")).click();
-    }
 
-    public void returnToGroupPage() {
-      driver.findElement(By.linkText("group page")).click();
-    }
-
-    public void submitGroupCreation() {
-      driver.findElement(By.name("submit")).click();
-    }
-
-    public void fillGroupForm(GroupData groupData) {
-      driver.findElement(By.name("group_name")).click();
-      driver.findElement(By.name("group_name")).clear();
-      driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-      driver.findElement(By.name("group_header")).click();
-      driver.findElement(By.name("group_header")).clear();
-      driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-      driver.findElement(By.name("group_footer")).click();
-      driver.findElement(By.name("group_footer")).clear();
-      driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
-
-    public void initGroupCreation() {
-      driver.findElement(By.name("new")).click();
-    }
-
-    public void goToGroupPage() {
-      driver.findElement(By.linkText("groups")).click();
-    }
 
     public void stop() {
         driver.quit();
     }
 
-    private boolean isAlertPresent() {
-      try {
-        driver.switchTo().alert();
-        return true;
-      } catch (NoAlertPresentException e) {
-        return false;
-      }
+
+
+    public GroupHelper getGroupHelper() {
+        return groupHelper;
     }
 
-    public void deleteSelectedGroups() {
-      driver.findElement(By.name("delete")).click();
-    }
-
-    public void selectGroup() {
-      driver.findElement(By.name("selected[]")).click();
+    public NavigationHelper getNavigationHelper() {
+        return navigationHelper;
     }
 }
