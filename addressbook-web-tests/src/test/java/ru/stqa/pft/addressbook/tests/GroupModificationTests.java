e7ee999f19f9f51b294b131e7b07a2f277d14514
DaniilAutoQA <daniil-bond@yandex.ru>
2020-01-11 11:36:24 +0300

    создан тест без рекордера
    созден тест редактирования групп

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification(){
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("edit1","Edit2","edit3"));
+        app.getGroupHelper().submitGroupModification();
+    }
+}
