2d80dacdeabf1ab3a98db7545e570cd14ba483bd
DaniilAutoQA <daniil-bond@yandex.ru>
2020-03-02 14:39:48 +0300

    сортировка списка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -18,6 +19,7 @@ public class GroupCreationTests extends TestBase {
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size() + 1);
+    before.add(group);
     //поиск максимального id
     /*
     int max = 0;
@@ -27,8 +29,11 @@ public class GroupCreationTests extends TestBase {
       }
     } */
     ////поиск максимального id с помощью лямбда выражения
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-    before.add(group);
+  //  group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    // Сортируем и затем проверяем списки
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
    }
 }
